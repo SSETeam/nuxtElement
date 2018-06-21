835024ab51bed566e967ac17bbf976aff4b7504b
zhangjingge <1103610594@qq.com>
2018-06-21 14:56:34 +0800

    脚手架搭建

---
+++ layouts/default.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <header>
+      <div class="top-bar clearfix">
+        <div class="top-bar-l">
+          欢迎您！<nuxt-link to='./bonduserinfo'>张三<i class="el-icon-edit"></i></nuxt-link>
+        </div>
+        <div class="top-bar-r">
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              张三<i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </div>
+      <div class="top-logo">
+        <div class="top-logo-bg">
+          <h1>
+            <img alt="logo" src="../assets/images/logo.png"/>
+          </h1>
+        </div>
+      </div>
+      <el-menu :default-active="activeIndex" class="blb-menu" mode="horizontal" 
+      @select="handleSelect"
+      active-text-color="#1890ff">
+        <el-menu-item index="1">首页</el-menu-item>
+        <el-submenu index="2" popper-class="blb-menu-item">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="3">消息中心</el-menu-item>
+      </el-menu>
+    </header>
+      <div class="blb-breadcrumb">
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item>首页</el-breadcrumb-item>
+          <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <nuxt/>
+      <footer class="blb-footer">
+         <div>窗前明月光，关顾感光鼓逛逛逛光光光光光</div>
+         <div>窗前明月光，关顾感光鼓逛逛逛光光光光光</div>
+         <div>窗前明月光，关顾感光鼓逛逛逛光光光光光</div>
+      </footer>
+  </div>
+
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+
---
+++ pages/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <section class="container">
+     <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
+        <el-tab-pane label="用户管理" name="first">
+          <el-table
+          border
+          :data="tableData"
+          style="width: 100%">
+          <el-table-column
+            label="日期"
+            width="180">
+            <template slot-scope="scope">
+              <span v-if="scope.$index!=editKey">
+                <i class="el-icon-time"></i>
+                <span style="margin-left: 10px">{{ scope.row.date }}</span>
+              </span>
+              <span v-else>
+                <el-input
+                  placeholder="请输入内容"
+                  v-model="input10"
+                  clearable>
+                </el-input>
+              </span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="姓名"
+            width="180">
+            <template slot-scope="scope">
+              <el-popover trigger="hover" placement="top">
+                <p>姓名: {{ scope.row.name }}</p>
+                <p>住址: {{ scope.row.address }}</p>
+                <div slot="reference" class="name-wrapper">
+                  <el-tag size="medium">{{ scope.row.name }}</el-tag>
+                </div>
+              </el-popover>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+              <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+        <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+        <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+      </el-tabs>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  data() {
+    return {
+      activeName2: 'first',
+      input10: '',
+      editable: true,
+      editKey: null,
+      tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
+    handleEdit(index, row) {
+      this.editable = false
+      this.editKey = index
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+
+
+</style>
+
